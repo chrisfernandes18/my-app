e8220cf9629a1b5c7d58db00441a083585942562
Christian Bjerre-Fernandes <chrisfernandes18@uchicago.edu>
2020-03-20 01:34:59 -0500

    added to the home page

--- src/Home.js
+++ src/Home.js
@@ -10,17 +10,17 @@ function Home(props) {
       <img src={finnJake} width="250" height="200" alt="Finn and Jake"></img>
       <div className="paragraph">
         <p>
-          Hello! My name is Christian Bjerre-Fernandes. I am currently a Third
-          year student at the <a href="https://www.uchicago.edu/">University of Chicago</a>,
-          studying Computer Science. I also love to study languages, and learning about other
-          cultures.
+          Hello! My name is Christian Bjerre-Fernandes. I am currently a third-year 
+          student at the <a href="https://www.uchicago.edu/">University of Chicago</a>,
+          studying computer science. In addition to computer science, I love to study languages, 
+          and learn about other cultures.
           <br></br>
           <br></br>
-          Some of the things I do on campus include being a part of <a href="https://equalopportunityprograms.uchicago.edu/title-ix/rsvp-programming-center/">Resources for Sexual Violence Prevention (RSVP)
+          On campus, I am a part of <a href="https://equalopportunityprograms.uchicago.edu/title-ix/rsvp-programming-center/">Resources for Sexual Violence Prevention (RSVP)
           </a>, <a href="https://www.facebook.com/UChicagoTheMark/">The Mark</a>, and <a href="https://uncommonhacks.com/">Uncommon Hacks</a>.
-          My hobbies include participating in Intramural Sports with my house (Ultimate Frisbee, Foosball, Inner Tube Waterpolo, etc),
-          playing Super Smash Bros Ultimate, watching Movies (The Prestige, Lincoln Lawyer, etc.) and Shows (Bojack Horseman, Love is Blind, etc.),
-          and Photography.
+          Besides school and work, I love to participate in intramural sports with my house (Ultimate Frisbee, Foosball, Inner Tube Waterpolo, etc),
+          play super smash bros ultimate (SSBU), watch movies (The Prestige, Lincoln Lawyer, etc.) and shows (Bojack Horseman, Love is Blind, etc.),
+          and photography.
           <br></br>
           <br></br>
           <a href={pdf} target="_blanks">Here</a> is my resume!
