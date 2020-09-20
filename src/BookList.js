62db67fcee75d93e22500e7c5936620f8bd23e8d
Christian Bjerre-Fernandes <chrisfernandes18@uchicago.edu>
2020-09-19 19:48:34 -0500

    finished booklist for now

--- src/BookList.js
+++ src/BookList.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import booklist from './assets/booklist.json';
 import './App.css';
 
-function BookList(props) {
-    return (
-        <div>
-            <h1>Book List</h1>
-        </div>
-    )
-}
+class BookList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
 
+    sortJSON() { 
+        /*
+        console.log(booklist.Read.sort((a, b) => a.Author < b.Author));
+        console.log(booklist.Read);
+        */
+    }
+
+    render() {
+        this.sortJSON()
+        return (
+            <div className="BookList">
+                <h1>Book List</h1>
+                <h2>Read</h2>
+                <ul alt="Books I have read already">
+                    {
+                        booklist.Read.map(book => <li key={book.Title} className="list"><b>{book.Title}</b>{''.concat(" by: ", book.Author)}</li>)
+                    }
+                </ul>
+                <h2>Want to Read</h2>
+                <ul alt="Want to read">
+                    {
+                        booklist.Want_to_Read.map(book => <li key={book.Title} className="list"><b>{book.Title}</b>{''.concat(" by: ", book.Author)}</li>)
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
 export default BookList;
